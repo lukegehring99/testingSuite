31be3ac2111153b992adb41cd4de87dbc3aca5f1
Luke Gehring <lukegehring99@gmail.com>
2018-06-19 21:32:25 -0500

    Testing python integration with SQL

---
+++ resources/data/setup.py
@@ -0,0 +1,19 @@
+# Check for mySQL server at local host
+# Read files in from sources.txt in the folloring format:
+# uniqueID - description - source - keyword1, keyword2, ... , keywordn -
+# check to see if the ID is new
+# if it is new add a new table
+# else continue
+# on exit
+
+
+import MySQLdb
+
+db = MySQLdb.commect(host="localhost",
+                     user="root",
+                     passwd="password",
+                     db="test")
+
+cur = db.cursor()
+
+db.close()
