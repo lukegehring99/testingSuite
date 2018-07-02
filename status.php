a97a6d9bb66cd81568231ec68f5783bf122d97ca
Luke Gehring <lukegehring99@gmail.com>
2018-07-01 21:37:53 -0500

    Testing charts for server load data

---
+++ status.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <script type="text/javascript" src="js/smoothie.js"></script>
+
+
+</head>
+
+<body onload="init()">
+  <canvas id="cpu" width="400" height="100"></canvas>
+  <canvas id="ram" width="400" height="100"></canvas>
+
+
+</body>
+</html>
+<?php
+
+
+
+?>
