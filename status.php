20b759874b22cb30179ed37dedc945ccbb7655b8
Luke Gehring <lukegehring99@gmail.com>
2018-07-01 21:46:51 -0500

    Attempting to get cpu usage

--- status.php
+++ status.php
@@ -2,6 +2,7 @@
 <html>
 <head>
   <script type="text/javascript" src="js/smoothie.js"></script>
+  <script type="text/javascript" src="js/load.js"></script>
 
 
 </head>
@@ -9,6 +10,19 @@
 <body onload="init()">
   <canvas id="cpu" width="400" height="100"></canvas>
   <canvas id="ram" width="400" height="100"></canvas>
+
+<div>
+
+  <?php
+  for($i = 0; $i < 20; $i++) {
+    $load = sys_getloadavg();
+    echo $load."    ";
+  }
+
+
+  ?>
+
+</div>
 
 
 </body>
