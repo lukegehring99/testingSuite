fa504aea74d49367ba08702a6540b8a0cbd00b78
Luke Gehring <lukegehring99@gmail.com>
2018-07-07 12:48:54 -0500

    Format of parser completed

---
+++ js/main.js
@@ -0,0 +1,3 @@
+function toggleDisplay(id) {
+    document.getElementById(id).classList.toggle("show");
+}
--- js/parse.js
+++ js/parse.js
@@ -19,20 +19,25 @@ function validUrl(url) {
   return url == '';
 }
 
+var counter = 0;
+
 function getAPI() {
+  document.getElementById('response-display').innerHTML = "";
   var sourceUrl = document.getElementById('apiurl').value;
 
-  if validUrl(sourceUrl){
+  if (validUrl(sourceUrl)){
     $.ajax({
       url: sourceUrl,
       dataType: "json",
       success: function(data) {
-        for(var key in data) {
+        /*for(var key in data) {
           if (data.hasOwnProperty(key)) {
             processEntry(key, data[key]);
 
           }
         }
+        */
+        processEntry(null, data, document.getElementById('response-display'));
         // potentially change this to a single call top processEntry with key=null and value=data
       }
     });
@@ -43,31 +48,64 @@ function getAPI() {
 }
 
 
-function processEntry(key, value) {
+function processEntry(key, value, parent) {
   var type = typeof(value);
   switch(type) {
     case "string":
-      break;
     case "number":
-      break;
     case "boolean":
+      diplayField(key, value, parent);
       break;
     case "object":
       if (Array.isArray(value)) {
-        displayArray(key, value);
+        displayArray(key, value, parent);
       } else {
-        for(var key2 in value) {
-          if(data.hasOwnProperty(key2))
-          processEntry(key2, value[key2])
+        for(var key1 in value) {
+          // Create the dropdown button element
+          var select = document.createElement("button");
+          select.class = "dropfield";
+          var unid = "field" + coutner;
+          counter++;
+          select.id = unid;
+          select.onclick = "toggleDisplay(" + unid + ")";
+          if(!(key == null)) {
+            select.innerHTML = key;
+          } else {
+            select.innerHTML = "root";
+          }
+          parent.appendChild(select);
+
+
+          if(data.hasOwnProperty(key1)) {
+            processEntry(key1, value[key1], select)
+          }
         }
       }
       break;
   }
 }
 
-function displayField(key, value) {
+function displayField(key, value, parent) {
 
+  var table = document.createElement("table");
+  var tr = document.createElement("tr");
+  var val = document.createElement("td");
+  var box = document.createElement("td");
+  var check = document.createElement("input");
+
+  check.type = "checkbox";
+  val.innerHTML = key + "  : " + value;
+
+  box.appendChild(check);
+  tr.appendChild(val);
+  tr.appendChild(box);
+  table.appendChild(tr);
+  parent.appendChild(table);
 }
-function displayArray(key, value) {
 
+function displayArray(key, value, parent) {
+  if(value.length > 0) {
+    key = "[" + key +"]";
+    processEntry(key, value[0], parent);
+  }
 }
